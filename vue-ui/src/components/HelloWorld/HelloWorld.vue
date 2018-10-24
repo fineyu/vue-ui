d4b229ccb8be61c80bc2893b0b390cdf5fc98b8f
ycc <you@example.com>
2018-10-24 16:45:29 +0800

    first

---
+++ vue-ui/src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div class="top">{{title}}</div>
+    <div v-for="item in items">
+      <div>
+        <img src="item.src" alt="">
+        <!-- <p>{{value}}</p> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      title:'Direct From Manufacturer',
+      items:[
+        {"src":'./images/vue1.jpg'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" src="./HelloWorld.less"></style>
